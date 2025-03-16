a5accc0b167821fbabcf44120e2640fd87228469
Emma <emma2334@gmail.com>
2025-03-17 00:31:40 +0800

    feat: create amount input

---
+++ src/components/payment/AmountInput.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { defineModel } from 'vue';
+
+defineProps<{
+  placeholder?: string;
+}>();
+
+const amount = defineModel();
+function handleKeydown(e: KeyboardEvent) {
+  const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'];
+  const { innerText = '' } = e.target as HTMLInputElement;
+
+  const newInput = innerText + e.key;
+  if (!allowedKeys.includes(e.key) && isNaN(Number(newInput))) {
+    e.preventDefault();
+    return false;
+  }
+}
+
+function handleInput(e: Event) {
+  amount.value = (e.target as HTMLInputElement).innerText;
+}
+</script>
+
+<template>
+  <div>
+    <span
+      class="text-7xl text-weight-bold"
+      role="textbox"
+      contenteditable
+      @keydown="handleKeydown"
+      @input="handleInput"
+      :placeholder="placeholder"
+    >
+      {{ amount }}
+    </span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+[contenteditable]:focus {
+  outline: none;
+  border-bottom: solid 2px $gray-900;
+}
+
+[contenteditable]:before {
+  content: '$';
+  margin-right: 0.25rem;
+  vertical-align: text-top;
+  font-size: map-get($font-sizes, 4xl);
+}
+
+[contenteditable]:empty {
+  color: $gray-100;
+  &:after {
+    content: attr(placeholder);
+    pointer-events: none;
+    font-weight: 900;
+  }
+}
+</style>
