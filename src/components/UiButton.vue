98d43a0ab406d256be106c2f21f920741092734a
Emma <emma2334@gmail.com>
2025-03-19 03:09:13 +0800

    feat: create button

---
+++ src/components/UiButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  icon?: string;
+  light?: boolean;
+}>();
+</script>
+
+<template>
+  <q-btn :class="['btn', { light }]" :label="label" :icon="icon" />
+</template>
+
+<style scoped lang="scss">
+.btn {
+  background-color: $orange-300;
+  color: white;
+
+  &.light {
+    background-color: $orange-50;
+    color: $orange-400;
+  }
+}
+</style>
