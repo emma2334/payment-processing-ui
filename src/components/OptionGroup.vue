fbf29dc164278c22e38191848f88ca32ea0672dd
Emma <emma2334@gmail.com>
2025-03-19 02:16:11 +0800

    feat: create button group

---
+++ src/components/OptionGroup.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  options: {
+    value: string;
+    label: string;
+    icon?: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <q-btn-toggle :options="options.map(({ value }) => ({ value, slot: value }))">
+    <template
+      v-for="option in options"
+      :key="option.value"
+      v-slot:[option.value]
+    >
+      <div class="row items-center no-wrap">
+        <q-icon v-if="option.icon" left :name="option.icon" />
+        <div class="text-center">{{ option.label }}</div>
+      </div>
+    </template>
+  </q-btn-toggle>
+</template>
+
+<style scoped lang="scss">
+:deep(button) {
+  color: $teal-700 !important;
+  --fa-primary-color: $teal-700;
+  --fa-secondary-color: $teal-700;
+
+  &[aria-pressed='true'] {
+    background-color: $teal-100 !important;
+  }
+
+  &.q-btn--outline:before {
+    border-color: $teal-100 !important;
+  }
+}
+</style>
