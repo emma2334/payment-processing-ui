3f7e073a796b2a2fa76d59e557990c82a704000f
Emma <emma2334@gmail.com>
2025-03-20 01:50:27 +0800

    chore: add btn icon color

--- src/components/UiButton.vue
+++ src/components/UiButton.vue
@@ -17,20 +17,25 @@ defineProps<{
 .btn {
   background-color: $orange-300;
   color: white;
+  --fa-primary-color: white;
+  --fa-secondary-color: white;
 
   &.light {
     background-color: $orange-50;
     color: $orange-400;
+    --fa-primary-color: $orange-400;
+    --fa-secondary-color: $orange-400;
   }
 }
 
 .btn.teal {
   background-color: $teal-300;
-  color: white;
 
   &.light {
     background-color: $teal-50;
-    color: $teal-400;
+    color: $teal-900;
+    --fa-primary-color: $teal-900;
+    --fa-secondary-color: $teal-900;
   }
 }
 </style>
